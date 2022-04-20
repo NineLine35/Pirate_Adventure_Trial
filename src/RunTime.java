7a9b7fb0bf866197b89935c25c94470d77027485
NineLine35 <65030521+NineLine35@users.noreply.github.com>
2022-04-20 17:34:55 -0500

    * Got the game play loop classes to work through movement (sailing) to different landclasses.  There is a placeholder in the AtSeaLoop and IslandLoop, however opening them FINALLY works.
    * Made adjustments to the Player class, including making the player a singleton.
    
    * Various other stuff

--- src/RunTime.java
+++ src/RunTime.java
@@ -1,10 +1,7 @@
 import GameLoop.OpeningLoop;
 import MapFiles.Landmass;
 import MapFiles.MainMap;
-
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class RunTime {
 
@@ -16,43 +13,12 @@ public class RunTime {
     // Launch opening game play loop
     OpeningLoop.launch(playMap);
 
-    // Receive user input and parse it
-      Scanner userInput = new Scanner(System.in);
 
-    // Game loop open while input stream is active
-      while(userInput.hasNext()){
+   }
 
-        // Store user input in variable
-        String enteredText = userInput.nextLine();
-
-        // Verify if user typed "exit".  If so, close the program
-        if(enteredText.toLowerCase().contains("exit")){
-          System.out.println("Good bye!");
-          System.exit(0);
-        }
-        else if(enteredText.toLowerCase().contains("map")){
-
-          //Initiate current player location
-          MainMap.Coordinates startLocation = new MainMap.Coordinates(2,2);
-          MainMap.Coordinates currentLocation = startLocation;
+ }
 
 
-          //DEBUG print out map to console
-          System.out.println("______MAP______");
-          System.out.println(Arrays.deepToString(playMap).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
-          System.out.println("_______________");
-
-        }
-        else {
-          System.out.println("Not recognized!  We have not implemented commands.  Try 'EXIT'....");
-        }
-
-
-      }
-
-  }
-
-}
 
 
 
