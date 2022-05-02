8f875cf9662375e70101eacd3b94c465ad4aba7b
NineLine35 <65030521+NineLine35@users.noreply.github.com>
2022-05-02 16:08:37 -0500

    * Added shipCombat starter code (using many of the Functional Programming requirements)  Still need to make the connections....but I have ideas :)

--- src/Ship/Ship.java
+++ src/Ship/Ship.java
@@ -3,6 +3,8 @@ package Ship;
 import Inventory.Item;
 import Inventory.ItemTypes;
 
+import java.util.Random;
+
 public abstract class Ship {
 
     private int hullHitPoints;
@@ -13,6 +15,7 @@ public abstract class Ship {
     private final int maxSailHealth = 100;
     private boolean isDestroyed = false;
     private String shipName;
+    private int fireDamage = 0;
 
 
     public Ship(int hullHitPoints, int cargoCapacity, int sailDistance) {
@@ -67,6 +70,33 @@ public abstract class Ship {
 
     public void setSailHealth(int sailHealth){ this.sailHealth = sailHealth; }
 
+    public int getMaxHullHitPoints() {
+        return maxHullHitPoints;
+    }
+
+    public int getFireDamage() {
+        return fireDamage;
+    }
+
+    public void setFireDamage(int fireDamage) {
+        this.fireDamage = fireDamage;
+    }
+
+    /**
+     * Method to remove an "x" amount of hit points from the hull due to damage, including fire damage
+     */
+    public void hullDamage(int x){
+        setHullHitPoints(hullHitPoints - (x + (fireDamage/2)));
+
+    }
+
+    /**
+     * Method to remove an "x" amount of hit points from the sail due to damage
+     */
+    public void sailDamage(double x){
+        setSailHealth((int) (sailHealth - x));
+    }
+
     public void repairShip(Item item){
         if (item.getName() == ItemTypes.PLANK_NAILS.toString()){
             setHullHitPoints(maxHullHitPoints);
@@ -75,6 +105,21 @@ public abstract class Ship {
         else if (item.getName() == ItemTypes.PATCHING_KIT.toString()){
             setSailHealth(maxSailHealth);
             System.out.println("You Have Repaired Your Sail To Full Health");
+        }
+    }
+
+    /**
+     * Simple method to calculate a fire chance on the ship, and if true set random damage value
+     * @return true if a fire breaks out
+     */
+    public boolean fireChance(int spread){
+        Random rand = new Random();
+        if(rand.nextInt(10) > 6){
+            setFireDamage((rand.nextInt(4)) * spread);
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
